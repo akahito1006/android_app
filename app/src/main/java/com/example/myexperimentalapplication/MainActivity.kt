ad4c64eaa0a37bcd4d81ab042913a6325efb5d0b
akahito1006 <nekodora5k@gmail.com>
2021-08-01 10:19:21 +0900

    override textview

--- app/src/main/java/com/example/myexperimentalapplication/MainActivity.kt
+++ app/src/main/java/com/example/myexperimentalapplication/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.myexperimentalapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val textView: TextView = findViewById(R.id.textArea)
+        textView.text = "modified text"
     }
 }
---
+++ app/src/main/java/com/example/myexperimentalapplication/SubComponent.java
@@ -0,0 +1,4 @@
+package com.example.myexperimentalapplication;
+
+public class SubComponent {
+}
