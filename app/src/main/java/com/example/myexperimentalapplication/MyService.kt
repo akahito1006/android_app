4017e6afcf498c91925db5225913d63418b65063
akahito1006 <nekodora5k@gmail.com>
2021-08-29 15:38:04 +0900

    Retrofitクラスのインスタンス化

--- app/src/main/java/com/example/myexperimentalapplication/MyService.kt
+++ app/src/main/java/com/example/myexperimentalapplication/MyService.kt
@@ -3,8 +3,10 @@ package com.example.myexperimentalapplication
 import okhttp3.Call
 import okhttp3.RequestBody
 import okhttp3.ResponseBody
+import retrofit2.Retrofit
 import retrofit2.http.*
 
+// Serviceの準備
 interface MyService {
 
     @GET("posts")
@@ -19,4 +21,14 @@ interface MyService {
     @DELETE("posts/{id}")
     fun deletePathParam(@Path("id") id:String): Call<ResponseBody>
 
+    // JSON文字列として取得するには、RequestBosyやResponseBodyはOKHttpで提供されているクラスを利用する
+    // 戻り値はCall型にし、サーバーから受け取るデータの型をジェネリクスで指定する
 }
+
+// Retroクラスのインスタンス化
+//      Retrofit本体
+private val retrofit = Retrofit.Builder().apply {
+    // baseUrlプロパティはokhttp3.HttpUrlでもOK
+    baseUrl("http://10.0.2.2:3000/")
+// buildメソッドを呼び出しインスタンスを取得する
+}.build()
