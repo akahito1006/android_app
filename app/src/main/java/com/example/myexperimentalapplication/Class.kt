1b492d80f440e8fd01e008b17f0bd0c9ac849c56
akahito1006 <nekodora5k@gmail.com>
2021-09-05 19:07:55 +0900

    val cannot be reassigned

--- app/src/main/java/com/example/myexperimentalapplication/Class.kt
+++ app/src/main/java/com/example/myexperimentalapplication/Class.kt
@@ -5,19 +5,24 @@ class Class
 // the visibility is set to "public" by default
 class User(var email: String, password: String, var age: Int) {
 
-    var password: String = password
+    var password: String = password // "field"
         get() { return field }
         set(value) { field = value }
 }
 
 fun main(args: Array<String>) {
 
+    // hot to use class as an object on Kotlin
     val user = User("hello@email.com", "azerty", 27)
 
     // Setter; to modify the value
     user.password = "123456"
     // Getter; to get the value
     user.password
+
+    // this modification is denied because of using the keyword "val"; immutable
+    user.email = "" // val cannot be reassigned
+
 }
 
 // Kotlin classはデフォルトでpublic
@@ -29,6 +34,6 @@ fun main(args: Array<String>) {
 // internal:    inside the same module; set of Kotlin files compiled together (gradle, Maven)
 // public:      visible everywhere by everyone
 
-// hot to use class as an object on Kotlin
+
 val user = User("hello@email.com", "azerty", 27)
 
