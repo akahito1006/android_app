cd4e415a933c43aefa4092aebdd7cf881cae51eb
akahito1006 <nekodora5k@gmail.com>
2021-09-05 18:59:12 +0900

    getter and setter

--- app/src/main/java/com/example/myexperimentalapplication/Class.kt
+++ app/src/main/java/com/example/myexperimentalapplication/Class.kt
@@ -1,18 +1,34 @@
 package com.example.myexperimentalapplication
 
-class
-class {
+class Class
+
+// the visibility is set to "public" by default
+class User(var email: String, password: String, var age: Int) {
+
+    var password: String = password
+        get() { return field }
+        set(value) { field = value }
+}
+
+fun main(args: Array<String>) {
+
+    val user = User("hello@email.com", "azerty", 27)
+
+    // Setter; to modify the value
+    user.password = "123456"
+    // Getter; to get the value
+    user.password
 }
 
 // Kotlin classはデフォルトでpublic
 
 // four main visibility modifiers for the members
 
-// private: only be inside the class
-// protected: only be inside the class and its subClass
-// internal: only be inside the same module; set of Kotlin files compiled together (gradle, Maven)
-// public: visible everywhere by everyone
+// private:     inside the class
+// protected:   inside the class and its subClass
+// internal:    inside the same module; set of Kotlin files compiled together (gradle, Maven)
+// public:      visible everywhere by everyone
 
 // hot to use class as an object on Kotlin
-val user = User("hello@email.com", "azerty", "27")
+val user = User("hello@email.com", "azerty", 27)
 
