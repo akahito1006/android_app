9155483e88ec38f56632f7e54e1185aa005e57d2
akahito1006 <nekodora5k@gmail.com>
2021-09-05 18:48:07 +0900

    Kotlin Class

---
+++ app/src/main/java/com/example/myexperimentalapplication/Class.kt
@@ -0,0 +1,14 @@
+package com.example.myexperimentalapplication
+
+class
+class {
+}
+
+// Kotlin classはデフォルトでpublic
+
+// four main visibility modifiers for the members
+
+// private: only be inside the class
+// protected: only be inside the class and its subClass
+// internal: only be inside the same module; set of Kotlin files compiled together (gradle, Maven)
+// public: visible everywhere by everyone
